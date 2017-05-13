3025e088d9d5d61ee6ba1287d93feac3c7ef3b42
monir <mkashkoul@gmail.com>
2017-05-13 11:02:24 +0200

    this lesson

--- src/main/java/org/redi/inheritance/ConstructorTest.java
+++ src/main/java/org/redi/inheritance/ConstructorTest.java
@@ -10,10 +10,10 @@ public class ConstructorTest {
     Person person1=new Person();
 
         person1.setFirstName("Monir");
-        System.out.print("the name of the guy is "+person1.getFirstName());
+        System.out.println("the name of the guy is "+person1.getFirstName());
         Person person2=new Person("Monir","lastname",new Date(),"mkashkoul");
         Student student1=new Student("Monir","kashkoul",new Date(),"mkash","javacourse");
-        System.out.print("the name of the student"+student1.getFirstName());
+        System.out.print("the name of the student is "+student1.getFirstName());
     }
 
 }
