a6f8b3ef59027ea2312c79ca476cb40c6b8b7e57
monir <mkashkoul@gmail.com>
2017-05-18 22:15:10 +0200

    this lesson

---
+++ src/main/java/org/redi/school/Location.java
@@ -0,0 +1,8 @@
+package org.redi.school;
+
+/**
+ * Created by redi on 5/18/17.
+ */
+public interface Location {
+    public abstract String city();
+}
---
+++ src/main/java/org/redi/school/Office.java
@@ -0,0 +1,7 @@
+package org.redi.school;
+
+/**
+ * Created by redi on 5/18/17.
+ */
+public class Office {
+}
